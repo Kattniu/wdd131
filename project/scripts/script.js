b965d596c35bda4e4c2eb7a98052bca161e55772
Kattniu <kathe.adriana24@gmail.com>
2024-10-28 19:00:01 +0100

    update

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -1,4 +1,29 @@
 
+const searchBar = document.querySelector('.search-bar input');
+const projectArticles = document.querySelectorAll('.project');
+
+searchBar.addEventListener('input', () => {
+    const searchTerm = searchBar.value.toLowerCase();
+    projectArticles.forEach(article => {
+        const text = article.innerText.toLowerCase();
+        article.style.display = text.includes(searchTerm) ? 'block' : 'none';
+    });
+});
+
+window.addEventListener('scroll', () => {
+    const projects = document.querySelectorAll('.project');
+    projects.forEach((project) => {
+        const projectPosition = project.getBoundingClientRect().top;
+        const screenPosition = window.innerHeight / 1.5;
+        if (projectPosition < screenPosition) {
+            project.classList.add('visible');
+        } else {
+            project.classList.remove('visible');
+        }
+    });
+});
+
+
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
 
